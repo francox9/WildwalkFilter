e1a62f871b5fd9154bb9824cb76895426b5e91eb
sunxufei92@gmail.com <sunxufei92@gmail.com>
2019-04-26 17:33:38 +1000

    wrap up for init version of extension

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { h, render } from 'preact';
-import './style.scss'
-import App from './App.jsx'
+import { h, render } from "preact";
+import "./style.scss";
+import App from "./App.jsx";
 
-const box = document.querySelector('.wt-boxes-container'),
-    container = document.createElement('div')
+const box = document.querySelector(".wt-boxes-container"),
+  container = document.createElement("div");
 
-box.insertBefore(container, box.firstChild)
+if (box) {
+  box.insertBefore(container, box.firstChild);
 
-render((
-    <App/>
-), container);
+  render(<App />, container);
+}
